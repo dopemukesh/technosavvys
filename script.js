dd30a6d5063a02d522c541c2c794e1e64e66942c
dopemukesh <dope.mukeshyadav@gmail.com>
2024-04-12 22:43:37 +0530

    some major changes are made like we add some images and added the sidebar too

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// Function for Sidebar
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-250px';
+    } else {
+        sidebar.style.left = '0px';
+    }
+}
+
+
+
+// Function for Sidebar Links
+const links = document.querySelectorAll('.links a');
+
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        // Remove 'active' class from all links
+        links.forEach(link => {
+            link.classList.remove('active');
+        });
+
+        // Add 'active' class to the clicked link
+        this.classList.add('active');
+    });
+});
