0e27e87d317b57f11d49fe2cd2757e8f6cce19c5
dopemukesh <dope.mukeshyadav@gmail.com>
2024-04-29 12:34:46 +0530

    today logo and colors are changed and some changes are made in styles related to sidebar

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,14 @@
 
 // Function for Sidebar
+const menuIcon = document.querySelector(".menu-icon img");
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     if (sidebar.style.left === '0px') {
-        sidebar.style.left = '-300px';
+        sidebar.style.left = '-250px';
+        menuIcon.src = "icons/menu-icon-bar.svg";
     } else {
         sidebar.style.left = '0px';
+        menuIcon.src = "icons/close-icon.svg";
     }
 }
 
